9bdff6d406f829dbc4d4798d9a7c8ffe77236470
aduarte95 <aduarte.roldan@gmail.com>
2019-07-01 00:57:04 -0600

    Reserva connection

---
+++ ELabs/Controllers/ReservationController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ELabs.Controllers
+{
+    public class ReservationController : ApiController
+    {
+        private Models.Reservation reservation;
+
+        public ReservationController()
+        {
+            this.reservation = new Models.Reservation();
+        }
+        // GET api/<controller>
+        [Route("reservas")]
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                String html = "";
+                foreach (reserva reserva in reservation.GetReservas())
+                {
+                    html += reserva.fecha_fin + "\n";
+                }
+                return Request.CreateResponse(HttpStatusCode.OK, html);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(e.Message);
+            }
+        }
+
+        // GET: api/Reservation/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Reservation
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Reservation/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Reservation/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ELabs/Models/Reservation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ELabs.Models
+{
+    public class Reservation
+    {
+        public List<reserva> GetReservas()
+        {
+            try
+            {
+                using (elabsDB db = new elabsDB())
+                {
+                    return db.reserva.ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new System.ArgumentException(e.Message);
+            }
+        }
+    }
+}
